0c597b03d4467d12ab3829c13845a95016e178f3
Haim Burla <haim.burla@sentrycs.com>
2025-12-10 09:57:23 +0200

    Made response title ediable

--- src/interaction/CommonInteractionResponses.ts
+++ src/interaction/CommonInteractionResponses.ts
@@ -2,9 +2,9 @@ import { InteractionResponse } from "./InteractionResponse";
 
 /** Contains predefined common responses and their ID. */
 export class CommonInteractionResponses {
-    static get ok() { return { id: 'ok', action: new InteractionResponse('ok', 'OK') }; }
-    static get cancel() { return { id: 'cancel', action: new InteractionResponse('cancel', 'Cancel') }; }
-    static get close() { return { id: 'close', action: new InteractionResponse('close', 'Close') }; }
-    static get yes() { return { id: 'yes', action: new InteractionResponse('yes', 'Yes') }; }
-    static get no() { return { id: 'no', action: new InteractionResponse('no', 'No') }; }
+    static readonly ok = { id: 'ok', action: new InteractionResponse('ok', 'OK') };
+    static readonly cancel = { id: 'cancel', action: new InteractionResponse('cancel', 'Cancel') };
+    static readonly close = { id: 'close', action: new InteractionResponse('close', 'Close') };
+    static readonly yes = { id: 'yes', action: new InteractionResponse('yes', 'Yes') };
+    static readonly no = { id: 'no', action: new InteractionResponse('no', 'No') };
 }
--- src/interaction/InteractionResponse.ts
+++ src/interaction/InteractionResponse.ts
@@ -1,13 +1,17 @@
+import { makeObservable, observable } from "mobx";
 import { RelayCommand } from "../commands/RelayCommand";
 
 /** Represents an action for the user to respond on an interaction request. */
 export class InteractionResponse {
+    /** The title of the response. */
+    @observable accessor title: string = '';
+
     constructor(
         /** The ID of the response. */
         readonly id: string,
 
         /** The title of the response. */
-        readonly title: string,
+        title: string,
 
         /** An optional command to invoke as the user responds. */
         readonly command?: RelayCommand<void>,
@@ -15,5 +19,7 @@ export class InteractionResponse {
         /** An optional icon. */
         readonly icon?: string
     ) {
+        this.title = title;
+        makeObservable(this);
     }
 }
