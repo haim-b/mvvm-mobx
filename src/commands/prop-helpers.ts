83c9bd4d85f4d46d9e3b769e47aeef8bfc2891d6
Haim Burla <haim.burla@sentrycs.com>
2025-12-17 14:19:08 +0200

    Added loading to command binding

--- src/commands/prop-helpers.ts
+++ src/commands/prop-helpers.ts
@@ -1,14 +1,17 @@
 import { RelayCommand } from './RelayCommand';
 
-export function bindCommand<T>(command: RelayCommand<T>, parameter: any = null, disabledPropName = 'disabled') {
+export function bindCommand<T>(command: RelayCommand<T>, parameter: any = null, config?: { disabledPropName?: string, loadingPropName?: string }) {
     if (!command) {
         return null;
     }
 
     const isDisabled = !command.canExecute(parameter);
+    const disabledPropName = config?.disabledPropName || 'disabled';
+    const loadingPropName = config?.loadingPropName || 'loading';
 
     return {
         onClick: () => command.execute(parameter),
         [disabledPropName]: isDisabled,
+        [loadingPropName]: command.workingFlag.isActive,
     };
 }
